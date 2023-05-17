2b90f0571188d96fab9a84975f0e672693b9de92
Fiora <fioratanbeilun@gmail.com>
2023-05-17 21:21:02 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Fiora Tan on 17/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
